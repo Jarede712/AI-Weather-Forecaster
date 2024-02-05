b2616c086e4fed0730067944fe5733566d624cd7
Jarede712 <jaredeic@yahoo.com>
2024-02-05 13:28:58 -0800

    added dotenv requirement to script file

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const { OpenAI } = require('@langchain/openai');
+require('dotenv').config();
 
 const model = new OpenAI({
   openAIApiKey: process.env.OPENAI_API_KEY,
   temperature: 0,
   model: 'gpt-3.5-turbo',
 });
+
+console.log({ model });
