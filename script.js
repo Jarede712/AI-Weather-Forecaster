bca6c3acc5006f0d4ba676512144d9c82655321d
Jarede712 <jaredeic@yahoo.com>
2024-02-05 14:08:48 -0800

    implemented prompt template and output parser to narrow down output from model

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const { OpenAI } = require('@langchain/openai');
 require('dotenv').config();
 const inquirer = require('inquirer');
+const { PromptTemplate } = require('@langchain/core/prompts');
+const { StructuredOutputParser } = require('langchain/output_parsers');
 
 const model = new OpenAI({
   openAIApiKey: process.env.OPENAI_API_KEY,
@@ -8,16 +10,32 @@ const model = new OpenAI({
   model: 'gpt-3.5-turbo',
 });
 
+const parser = StructuredOutputParser.fromNamesAndDescriptions({
+  Current_Temperature:
+    'The current temperature of the location specified by the user',
+  Forecast:
+    'Seven day weather forecast, broken down day by day for the user in the form of a concise chart',
+});
+
+const formatInstructions = parser.getFormatInstructions();
+
 const promptFunc = async (input) => {
   try {
-    const res = await model.call(input);
+    const prompt = new PromptTemplate({
+      template:
+        'You are an expert weather forecaster, when the user gives you, or asks about, a location, you will provide them with a breakdown of the seven day weather forecast for that location, as well as the current temperature of the location specified.\n{format_instructions}\n{question}',
+      inputVariables: ['question'],
+      partialVariables: { format_instructions: formatInstructions },
+    });
+    const promptInput = await prompt.format({
+      question: input,
+    });
+    const res = await model.call(promptInput);
     console.log(res);
   } catch (err) {
     console.error(err);
   }
 };
-
-promptFunc();
 
 const init = () => {
   inquirer
